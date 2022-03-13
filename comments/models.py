204abca5db059638c0f259f17eac5e76c86b4f48
JulianeGampe <juliane.gampe@gmail.com>
2022-03-13 17:38:08 +0000

    Create the comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'posts.Post', on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+    
