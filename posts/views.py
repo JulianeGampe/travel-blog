c5db8b70d7fdabb4533c6267102f6759a905061a
JulianeGampe <juliane.gampe@gmail.com>
2022-03-14 14:22:48 +0000

    Create view code for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+# class PostList(generic.ListView):
+#     model = Post
+#     queryset = Post.objects.filter(status=1).order_by('-created')
+#     template_name = 'posts.html'
+#     paginate_by = 3
+def posts(request):
+    return render(request, 'posts.html')
+    
