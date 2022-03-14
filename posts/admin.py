00ea4c774d74eda1d563fc638274cf1c88fa7a23
JulianeGampe <juliane.gampe@gmail.com>
2022-03-14 09:07:54 +0000

    Add filter, search, display, prepopulated fields

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created')
