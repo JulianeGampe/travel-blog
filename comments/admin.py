94744a1ddd5091caa9897c54af6c8c0fa9116e1f
JulianeGampe <juliane.gampe@gmail.com>
2022-03-14 09:19:33 +0000

    Set up comments in admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Comment
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created', 'approved')
+    list_filter = ('approved', 'created')
+    search_fields = ('name', 'email', 'body')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+        
