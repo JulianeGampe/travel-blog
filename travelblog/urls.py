5bb88e3b0c4eb8fc7045646766e3e230ee6d08b7
JulianeGampe <juliane.gampe@gmail.com>
2022-03-21 13:26:17 +0000

    Add path to display comments

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from posts import views
+from comments import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('posts/', views.posts, name='posts'),
+    path('<slug:slug>/', views.comments, name='comments'),
     path('accounts/', include('allauth.urls'))
 ]
