1b3d6038b023db00a9af4782f333dc57f4aa88a5
JulianeGampe <juliane.gampe@gmail.com>
2022-03-22 11:43:36 +0000

    Add the comments view, correct the indentation

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, redirect
 from .models import Post
+from comments.forms import CommentForm
 
-# Create your views here.
-# class PostList(generic.ListView):
-#     model = Post
-#     queryset = Post.objects.filter(status=1).order_by('-created')
-#     template_name = 'posts.html'
-#     paginate_by = 3
+
 def posts(request):
     posts = Post.objects.all()
     context = {
         'posts': posts
     }
     return render(request, 'posts/posts.html', context)
-    
+
+
+def comments(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post 
+            comment.save()
+
+            return redirect('comments', slug=post.slug)
+    else:
+        form = CommentForm()
+  
+    return render(
+        request, 'comments/comments.html', {'post': post, 'form': form}
+        )
