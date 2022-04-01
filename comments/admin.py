6e5a594957e3517102a1da277e4d1b66992944fc
JulianeGampe <juliane.gampe@gmail.com>
2022-04-01 07:46:49 +0000

    Add approve_comments action

--- comments/admin.py
+++ comments/admin.py
@@ -8,6 +8,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created', 'approved')
     list_filter = ('approved', 'created')
     search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
