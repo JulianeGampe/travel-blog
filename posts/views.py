3eb7cf139b5a34f5a7d5e984da3e6ea7b5727733
JulianeGampe <juliane.gampe@gmail.com>
2022-04-01 10:11:51 +0000

    Add view for likes

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Post
 from comments.forms import CommentForm
+from django.http import HttpResponseRedirect
 
 
 def posts(request):
@@ -16,6 +17,7 @@ def comments(request, slug):
     comments = post.comments.filter(approved=True).order_by("-created")
     new_comment = None
 
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
 
@@ -28,5 +30,24 @@ def comments(request, slug):
         form = CommentForm()
   
     return render(
-        request, 'comments/comments.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'form': form}
-        )
+        request, 
+        'comments/comments.html', 
+        {
+            'post': post, 
+            'comments': comments, 
+            'new_comment': new_comment, 
+            'form': form
+        }
+    )
+
+
+def likes(request,slug):
+    post = Post.objects.get(slug=slug)
+
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+
+    else:
+        post.likes.add(request.user)
+
+    return HttpResponseRedirect(reverse('comments', args=[slug]))
