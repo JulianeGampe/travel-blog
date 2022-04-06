fa3fbbadade799a4c74aa8bda74827aba86f5904
JulianeGampe <juliane.gampe@gmail.com>
2022-04-06 14:26:03 +0000

    Create the PostForm for the profile

---
+++ profile/forms.py
@@ -0,0 +1,9 @@
+from posts.models import Post
+from comments.models import Comment
+from django import forms
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'image', 'content', 'likes',)
+
