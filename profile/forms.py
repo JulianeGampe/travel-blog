a2a2dfea777c796be35bed1c08e8a1d0bea612d3
JulianeGampe <juliane.gampe@gmail.com>
2022-04-15 13:59:59 +0000

    Add cloudinary file field

--- profile/forms.py
+++ profile/forms.py
@@ -1,8 +1,14 @@
 from posts.models import Post
 from comments.models import Comment
 from django import forms
+from cloudinary.forms import CloudinaryFileField
 
 class PostForm(forms.ModelForm):
+    image = CloudinaryFileField(
+        options= {
+            'folder': 'travelblogproject'
+        }
+    )
     class Meta:
         model = Post
         fields = ('title', 'slug', 'status', 'image', 'content',)
