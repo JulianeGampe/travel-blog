f1048735bc201309f29ba0bb46dd1557a4190235
JulianeGampe <juliane.gampe@gmail.com>
2022-04-18 10:09:29 +0000

    Import and add tinymce for content field

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,12 @@
 from .models import Post
 from django import forms
+from tinymce.widgets import TinyMCE
 
 
 class PostEditForm(forms.ModelForm):
+
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 50, 'rows': 30}))
+
     class Meta:
         model = Post
         fields = ('title', 'slug', 'status', 'image', 'content',)
