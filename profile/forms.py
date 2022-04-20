07fa8abf80ec2101f41e90ef2e4c87d629949bc6
JulianeGampe <juliane.gampe@gmail.com>
2022-04-20 12:01:59 +0000

    Remove CloudinaryFileField

--- profile/forms.py
+++ profile/forms.py
@@ -4,16 +4,11 @@ from django import forms
 from cloudinary.forms import CloudinaryFileField
 from tinymce.widgets import TinyMCE
 
+
 class PostForm(forms.ModelForm):
-    image = CloudinaryFileField(
-        options= {
-            'folder': 'travelblogproject'
-        }
-    )
 
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 50, 'rows': 30}))
 
     class Meta:
         model = Post
         fields = ('title', 'slug', 'status', 'image', 'content',)
-
