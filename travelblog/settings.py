1e06e4e50e32aaf9636c8bdca56b24ae717f99f9
JulianeGampe <juliane.gampe@gmail.com>
2022-04-22 15:17:50 +0000

    Set x_frame_options to allow from ami responsive

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['travelblogproject.herokuapp.com', 'localhost']
 
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'ALLOW FROM http://ami.responsivedesign.is'
 
 
 # Application definition
