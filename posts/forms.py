fd68c39ae3d819d22571cc2f59782c0600e18777
JulianeGampe <juliane.gampe@gmail.com>
2022-04-24 18:16:32 +0000

    Add code comment

--- posts/forms.py
+++ posts/forms.py
@@ -4,10 +4,11 @@ from tinymce.widgets import TinyMCE
 
 
 class PostEditForm(forms.ModelForm):
-
+    '''
+    PostEditForm to edit the post on the frontend
+    '''
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 50, 'rows': 30}))
 
     class Meta:
         model = Post
         fields = ('title', 'slug', 'status', 'image', 'content',)
-        
