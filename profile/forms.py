57f9ce6946921442ac16acf2f2882c17488a3948
JulianeGampe <juliane.gampe@gmail.com>
2022-04-24 18:29:32 +0000

    Delete unnecessary imports and add code comment

--- profile/forms.py
+++ profile/forms.py
@@ -1,12 +1,12 @@
 from posts.models import Post
-from comments.models import Comment
 from django import forms
-from cloudinary.forms import CloudinaryFileField
 from tinymce.widgets import TinyMCE
 
 
 class PostForm(forms.ModelForm):
-
+    '''
+    PostForm to create a post from the frontend
+    '''
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 50, 'rows': 30}))
 
     class Meta:
