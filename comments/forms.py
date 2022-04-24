de551a1a92feb406bc3ecd51825b63a7007760e6
JulianeGampe <juliane.gampe@gmail.com>
2022-04-24 17:56:42 +0000

    Add code comments and delete white space

--- comments/forms.py
+++ comments/forms.py
@@ -3,7 +3,9 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    '''
+    CommentForm to create a new comment
+    '''
     class Meta:
         model = Comment
         fields = ('body',)
-        
