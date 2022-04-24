0354e93da6e293706b1a5b3b30963758a8d464d7
JulianeGampe <juliane.gampe@gmail.com>
2022-04-24 14:38:46 +0000

    Add code comments

--- comments/models.py
+++ comments/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 
-# Create your models here.
 class Comment(models.Model):
+    '''
+    Model for the comments of the posts
+    '''
     post = models.ForeignKey(
         'posts.Post', on_delete=models.CASCADE, related_name="comments"
     )
@@ -13,9 +15,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
+        '''
+        Sorts comments by date created
+        '''
         ordering = ["-created"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-
-    
