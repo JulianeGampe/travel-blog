67d5ebc7d46c1e7d38a5be9055c82a9c31c1d909
JulianeGampe <juliane.gampe@gmail.com>
2022-04-24 17:35:28 +0000

    Remove default django comment

--- aboutme/views.py
+++ aboutme/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def aboutme(request):
     template = 'aboutme/aboutme.html'
     context = {
+
     }
     return render(request, template, context)
